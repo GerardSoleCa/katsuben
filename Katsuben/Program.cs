e6098959e729077bb178f601947ed89866045969
Gerard Sole <g.sole.ca@gmail.com>
2020-06-08 22:48:27 +0200

    Program.cs: rename class and adapt for exit code

---
+++ Katsuben/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+
+namespace Katsuben
+{
+    public class Program
+    {
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "Specify the subtitle to convert")]
+            public string InputSubtitle { get; set; }
+
+            [Option('o', "output", Required = true, Separator = ' ',
+                HelpText = "Specify the output extensions separated by spaces. Optionally you can select the output encoding by appending them with a colon (srt:utf-8)")]
+            public IEnumerable<string> OutputSubtitles { get; set; }
+        }
+
+        public static int Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
+            return Parser.Default.ParseArguments<Options>(args).MapResult(
+                options =>
+                {
+                    var converter = new SubtitleConverter(SubtitleLoader.FromFile(options.InputSubtitle));
+                    options.OutputSubtitles.ToList().ForEach(o => converter.Convert(OutputSubtitle.FromArgument(o)));
+                    return 0;
+                },
+                errors => 1);
+        }
+
+        private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine($"Error: {((Exception)e.ExceptionObject).Message}");
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ TestKatsuben/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using Katsuben;
+using Xunit;
+
+namespace TestKastuben
+{
+    public class ProgramTests
+    {
+
+        [Theory]
+        [InlineData("-i o")]
+        public void Katsuben_FromArguments(string argument)
+        {
+            Program.Main(argument.Split(' '));
+        }
+    }
+}
